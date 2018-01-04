6bc594a4384acc1ab984e1db29165a5a79a86a6d
Will Dietz <w@wdtz.org>
2018-01-04 07:55:20 -0600

    gitlab.nix: maybe more readable

--- gitlab.nix
+++ gitlab.nix
@@ -1,11 +1,12 @@
 { lib }:
-  let
-    gitlabURL = "git@gitlab.engr.illinois.edu";
-    gitlabGenURL =
-      { group ? "llvm", repo, branch ? null }:
-        let
-          branchStr = lib.optionalString (branch != null) (" " + branch);
-        in
-        "${gitlabURL}:${group}/${repo}" + branchStr;
-    in args: lib.makeOverridable (a: { type = "git"; value = gitlabGenURL a; }) args
+let
+  gitlabURL = "git@gitlab.engr.illinois.edu";
+  gitlabGenURL =
+    { group ? "llvm", repo, branch ? null }:
+      let
+        branchStr = lib.optionalString (branch != null) (" " + branch);
+      in
+      "${gitlabURL}:${group}/${repo}" + branchStr;
+  gitspec = value: { type = "git"; inherit value; };
+in lib.makeOverridable (args: gitspec (gitlabGenURL args))
 
