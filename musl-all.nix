2e6ad41e9e7849cee3b8e576a260dbe850254b1e
Will Dietz <w@wdtz.org>
2018-01-04 19:59:33 -0600

    musl-all: build full 'release' set using cross
    Might be too much long-term, but let's see.

---
+++ musl-all.nix
@@ -0,0 +1,29 @@
+
+
+{ nixpkgs
+, supportedSystems ? [ "x86_64-linux" ]
+, # Strip most of attributes when evaluating to spare memory usage
+  scrubJobs ? true
+}:
+
+# For now, hijack release-cross.nix
+
+with import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems scrubJobs; };
+
+mapTestOnCross lib.systems.examples.musl64 ((packagePlatforms pkgs) // rec {
+      haskell.compiler = packagePlatforms pkgs.haskell.compiler;
+      haskellPackages = packagePlatforms pkgs.haskellPackages;
+
+      # Language packages disabled in https://github.com/NixOS/nixpkgs/commit/ccd1029f58a3bb9eca32d81bf3f33cb4be25cc66
+
+      #emacsPackagesNg = packagePlatforms pkgs.emacsPackagesNg;
+      #rPackages = packagePlatforms pkgs.rPackages;
+      ocamlPackages = { };
+      perlPackages = { };
+
+      darwin = packagePlatforms pkgs.darwin // {
+        cf-private = {};
+        osx_private_sdk = {};
+        xcode = {};
+      };
+    } )
