41d7621faee93748cf96b21952833d1cb34e1e30
Will Dietz <w@wdtz.org>
2018-01-11 15:49:27 -0600

    musl-all: changing purpose, grab much of release-lib.nix

--- musl-all.nix
+++ musl-all.nix
@@ -8,11 +8,65 @@
 ,  nixpkgsArgs ? { config = { allowUnfree = false; inHydra = true; }; }
 }:
 
-# For now, hijack release-cross.nix
 
-with import "${nixpkgs}/pkgs/top-level/release-lib.nix" { inherit supportedSystems scrubJobs nixpkgsArgs; };
+let
+  lib = import "${nixpkgs}/lib";
+  muslNixpkgsArgs = lib.recursiveUpdate { localSystem = lib.systems.examples.musl64; } nixpkgsArgs;
+  packageSet = import nixpkgs;
+in with lib;
 
-mapTestOnCross lib.systems.examples.musl64 ((packagePlatforms pkgs) // rec {
+with let
+in rec {
+  allPackages = args: packageSet (args // muslNixpkgsArgs);
+
+  hydraJob' = if scrubJobs then hydraJob else id;
+
+  # pkgs = packageSet (lib.recursiveUpdate { system = "x86_64-linux"; config.allowUnsupportedSystem = true; } nixpkgsArgs);
+  pkgs = allPackages { };
+  inherit lib;
+
+
+  forAllSupportedSystems = systems: f:
+    genAttrs (filter (x: elem x supportedSystems) systems) f;
+
+  /* Build a package on the given set of platforms.  The function `f'
+     is called for each supported platform with Nixpkgs for that
+     platform as an argument .  We return an attribute set containing
+     a derivation for each supported platform, i.e. ‘{ x86_64-linux =
+     f pkgs_x86_64_linux; i686-linux = f pkgs_i686_linux; ... }’. */
+  testOn = systems: f: forAllSupportedSystems systems
+    (system: hydraJob' (f (pkgsFor system)));
+
+
+  /* Similar to the testOn function, but with an additional
+     'crossSystem' parameter for allPackages, defining the target
+     platform for cross builds. */
+  testOnCross = crossSystem: systems: f: forAllSupportedSystems systems
+    (system: hydraJob' (f (allPackages { inherit system crossSystem; })));
+
+
+  /* Given a nested set where the leaf nodes are lists of platforms,
+     map each leaf node to `testOn [platforms...] (pkgs:
+     pkgs.<attrPath>)'. */
+  mapTestOn = mapAttrsRecursive
+    (path: systems: testOn systems (pkgs: getAttrFromPath path pkgs));
+
+  /* Recursively map a (nested) set of derivations to an isomorphic
+     set of meta.platforms values. */
+  packagePlatforms = mapAttrs (name: value:
+    let res = builtins.tryEval (
+      if isDerivation value then
+        value.meta.hydraPlatforms or (value.meta.platforms or [ "x86_64-linux" ])
+      else if value.recurseForDerivations or false || value.recurseForRelease or false then
+        packagePlatforms value
+      else
+        []);
+    in if res.success then res.value else []
+    );
+
+};
+
+mapTestOn ((packagePlatforms pkgs) // rec {
       haskell.compiler = packagePlatforms pkgs.haskell.compiler;
       haskellPackages = packagePlatforms pkgs.haskellPackages;
 
