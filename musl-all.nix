f81d17598d6c37c3c7a43c8034d7d1722fbe0bd1
Will Dietz <w@wdtz.org>
2018-01-11 20:08:04 -0600

    refactor musl-all into a release-lib-esque file

--- musl-all.nix
+++ musl-all.nix
@@ -8,76 +8,12 @@
 ,  nixpkgsArgs ? { config = { allowUnfree = false; inHydra = true; }; }
 }:
 
+let release-musl-native-lib = import ./release-musl-native-lib.nix { inherit supportedSystems scrubJobs nixpkgs nixpkgsArgs; };
 
-let
-  lib = import "${nixpkgs}/lib";
-  muslNixpkgsArgs = lib.recursiveUpdate { localSystem = lib.systems.examples.musl64; } nixpkgsArgs;
-  packageSet = import nixpkgs;
-in with lib;
+in
+  with release-musl-native-lib;
+  #  with import ./job-groups.nix release-lib;
 
-with rec {
-  allPackages = args: packageSet (lib.recursiveUpdate args muslNixpkgsArgs);
-
-  hydraJob' = if scrubJobs then hydraJob else id;
-
-  # pkgs = packageSet (lib.recursiveUpdate { system = "x86_64-linux"; config.allowUnsupportedSystem = true; } nixpkgsArgs);
-  pkgs = allPackages { config.allowUnsupportedSystems = true; };
-  inherit lib;
-
-  pkgsFor = system:
-    if system == "x86_64-linux" then pkgs #  pkgs_x86_64_linux
-    # else if system == "i686-linux" then pkgs_i686_linux
-    # else if system == "aarch64-linux" then pkgs_aarch64_linux
-    # else if system == "armv6l-linux" then pkgs_armv6l_linux
-    # else if system == "armv7l-linux" then pkgs_armv7l_linux
-    # else if system == "x86_64-darwin" then pkgs_x86_64_darwin
-    # else if system == "x86_64-freebsd" then pkgs_x86_64_freebsd
-    # else if system == "i686-freebsd" then pkgs_i686_freebsd
-    # else if system == "i686-cygwin" then pkgs_i686_cygwin
-    # else if system == "x86_64-cygwin" then pkgs_x86_64_cygwin
-    else abort "unsupported system type: ${system}";
-
-
-  forAllSupportedSystems = systems: f:
-    genAttrs (filter (x: elem x supportedSystems) systems) f;
-
-  # testOn = systems: f: hydraJob' (f pkgs);
-  /* Build a package on the given set of platforms.  The function `f'
-     is called for each supported platform with Nixpkgs for that
-     platform as an argument .  We return an attribute set containing
-     a derivation for each supported platform, i.e. ‘{ x86_64-linux =
-     f pkgs_x86_64_linux; i686-linux = f pkgs_i686_linux; ... }’. */
-  testOn = systems: f: forAllSupportedSystems systems
-    (system: hydraJob' (f (pkgsFor system)));
-
-
-  /* Similar to the testOn function, but with an additional
-     'crossSystem' parameter for allPackages, defining the target
-     platform for cross builds. */
-     #  testOnCross = crossSystem: systems: f: forAllSupportedSystems systems
-     #    (system: hydraJob' (f (allPackages { inherit system crossSystem; })));
-
-
-  /* Given a nested set where the leaf nodes are lists of platforms,
-     map each leaf node to `testOn [platforms...] (pkgs:
-     pkgs.<attrPath>)'. */
-  mapTestOn = mapAttrsRecursive
-    (path: systems: testOn systems (pkgs: getAttrFromPath path pkgs));
-
-  /* Recursively map a (nested) set of derivations to an isomorphic
-     set of meta.platforms values. */
-  packagePlatforms = mapAttrs (name: value:
-    let res = builtins.tryEval (
-      if isDerivation value then
-        value.meta.hydraPlatforms or (value.meta.platforms or [ "x86_64-linux" ])
-      else if value.recurseForDerivations or false || value.recurseForRelease or false then
-        packagePlatforms value
-      else
-        []);
-    in if res.success then res.value else []
-    );
-
-};
 
 mapTestOn ((packagePlatforms pkgs) // rec {
       haskell.compiler = packagePlatforms pkgs.haskell.compiler;
