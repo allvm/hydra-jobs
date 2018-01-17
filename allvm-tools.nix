d5dc45678303a46d341809bbe1e98fb0fa368e35
Will Dietz <w@wdtz.org>
2018-01-16 19:39:15 -0600

    allvm-tools: build cross and native

--- allvm-tools.nix
+++ allvm-tools.nix
@@ -3,22 +3,31 @@
 let
   config = { allowUnfree = false; };
   lib = import (nixpkgs + "/lib");
+  buildALLVMWith = pkgs: pkgs.callPackage ./support/allvm-tools {
+    src = allvm-tools-src;
+    clang-format = pkgs.buildPackages.clang.cc;
+    buildDocs = false;
+    # TODO: Enable this!
+    # (our cross-built LLVM's are built with full dependencies on dynamic libraries
+    #  that aren't included as allowed references such as ncursesw, etc.)
+    stripReferences = false;
+  };
+
   buildToolsFor = _: crossSystem:
-    let pkgs = import nixpkgs { inherit config crossSystem; };
-    in pkgs.callPackage ./support/allvm-tools {
-      src = allvm-tools-src;
-      clang-format = pkgs.buildPackages.clang.cc;
-      buildDocs = false;
-      # TODO: Enable this!
-      # (our cross-built LLVM's are built with full dependencies on dynamic libraries
-      #  that aren't included as allowed references such as ncursesw, etc.)
-      stripReferences = false;
-    };
-  in lib.mapAttrs buildToolsFor {
+    buildALLVMWith (import nixpkgs { inherit config crossSystem; });
+  buildToolsOn = _: localSystem:
+    buildALLVMWith (import nixpkgs { inherit config localSystem; });
+
+in {
+  allvm-tools-cross = lib.mapAttrs buildToolsFor {
     inherit (lib.systems.examples)
       musl64
       muslpi
       aarch64-multiplatform-musl
       ;
-  }
-
+  };
+  allvm-tools-native = lib.mapAttrs buildToolsOn {
+    inherit (lib.systems.examples) musl64;
+    #default = null;
+  };
+}
