0da4d19271817a355fdc5c28583162268bb23c53
Will Dietz <w@wdtz.org>
2018-02-09 17:13:04 -0600

    allvm-tools: minor cleanup

--- projects/allvm-tools.nix
+++ projects/allvm-tools.nix
@@ -4,7 +4,10 @@ let
   pkgs = import nixpkgs {};
   gitlab = import ../support/gitlab.nix { inherit (pkgs) lib; };
 
-  defaultSettings = import ../support/default-settings.nix { inherit (pkgs) lib; };
+  defaultdefaultSettings = import ../support/default-settings.nix { inherit (pkgs) lib; };
+  defaultSettings = defaultdefaultSettings // {
+    path = "jobset/allvm-tools.nix";
+  };
 
   ## Git repo definitions, aliases
 in with (import ../support/repos.nix { inherit (pkgs) lib; });
@@ -17,28 +20,23 @@ let
 
   jobsetsAttrs = with pkgs.lib; mapAttrs (name: settings: recursiveUpdate defaultSettings settings) (rec {
     allvm-tools-cross = {
-      path = "jobset/allvm-tools.nix";
       inputs.nixpkgs = nixpkgs-musl-pr;
       inputs.allvm-tools-src = allvm-tools;
     };
     allvm-tools-cross-next = {
-      path = "jobset/allvm-tools.nix";
       inputs.nixpkgs = nixpkgs-musl-next;
       inputs.allvm-tools-src = allvm-tools;
     };
     allvm-tools-cross-cleanup = {
-      path = "jobset/allvm-tools.nix";
       inputs.nixpkgs = nixpkgs-musl-cleanup;
       inputs.allvm-tools-src = allvm-tools;
     };
     allvm-tools-llvm5 = {
-      path = "jobset/allvm-tools.nix";
       inputs.nixpkgs = nixpkgs-musl-pr;
       inputs.allvm-tools-src = allvm-tools.override { branch = "experimental/llvm-5"; };
       inputs.llvmVersion = { type = "nix"; value = "5"; };
     };
     allvm-tools-llvm5-next = {
-      path = "jobset/allvm-tools.nix";
       inputs.nixpkgs = nixpkgs-musl-next;
       inputs.allvm-tools-src = allvm-tools.override { branch = "experimental/llvm-5"; };
       inputs.llvmVersion = { type = "nix"; value = "5"; };
