adfc1922278b2fee81c895fe3ca6c365800039e0
Will Dietz <w@wdtz.org>
2018-02-11 16:26:35 -0600

    cross: Test feature/musl-lib-rebase too?

--- projects/cross.nix
+++ projects/cross.nix
@@ -45,6 +45,7 @@ let
     // (jobsFor "cleanup" nixpkgs-musl-cleanup)
     // (jobsFor "PR" nixpkgs-musl-pr)
     // (jobsFor "next" nixpkgs-musl-next)
+    // (jobsFor "lib-rebase" (nixpkgs-musl-next.override { branch = "feature/musl-lib-rebase"; }))
     // rec {
     #bootstrap-tools = {
     #  keep = 2;
