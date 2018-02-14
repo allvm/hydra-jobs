028a56e985dc1aa4b8e450ce750dafeac04ac20c
Will Dietz <w@wdtz.org>
2018-02-14 12:39:12 -0600

    Revert "change hydra-jobs URI to use git:// instead of https://, testing"
    This reverts commit 6dc33b6ad7f2f1eef6bc5f26a810f25515e1fe28.

--- support/default-settings.nix
+++ support/default-settings.nix
@@ -10,7 +10,7 @@
   inputs = {
     jobs = {
       type = "git";
-      value = "git://github.com/allvm/hydra-jobs.git";
+      value = "https://github.com/allvm/hydra-jobs";
     };
     supportedSystems = {
       type = "nix";
