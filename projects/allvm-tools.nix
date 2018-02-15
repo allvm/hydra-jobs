ebe15475be86dd1ebd676b82a7c61cd70d4824c5
Will Dietz <w@wdtz.org>
2018-02-14 18:32:25 -0600

    allvm-tools: re-org, hopefully this is acceptable

--- projects/allvm-tools.nix
+++ projects/allvm-tools.nix
@@ -22,70 +22,98 @@ let
   };
 
   jobsetsAttrs = with pkgs.lib; mapAttrs (name: settings: recursiveUpdate defaultSettings settings) ({
-    allvm-tools = {
-      path = "default.nix";
-      input = "allvm-tools-src";
-      description = "ALLVM Tools (pinned nixpkgs)";
-      shares = 100;
+    tools = {
+      default = {
+        path = "default.nix";
+        input = "allvm-tools-src";
+        description = "ALLVM Tools (pinned nixpkgs)";
+        shares = 100;
+      };
+      with-nixpkgs-master = {
+        path = "default.nix";
+        input = "allvm-tools-src";
+        description = "ALLVM Tools (nixpkgs master)";
+        inputs.nixpkgs = nixpkgs-master;
+        interval = 60 * 60 * 4;
+      };
+
+      with-llvm5 = {
+        path = "default.nix";
+        input = "allvm-tools-src";
+        inputs.allvm-tools-src = allvm-tools-llvm5;
+        description = "ALLVM Tools - LLVM5 (pinned nixpkgs)";
+      };
+      with-llvm6 = {
+        path = "default.nix";
+        input = "allvm-tools-src";
+        inputs.allvm-tools-src = allvm-tools-llvm6;
+        description = "ALLVM Tools - LLVM6 (pinned nixpkgs)";
+      };
     };
-    allvm-tools-master = {
-      path = "default.nix";
-      input = "allvm-tools-src";
-      description = "ALLVM Tools (nixpkgs master)";
-      inputs.nixpkgs = nixpkgs-master;
-      interval = 60 * 60 * 4;
-    };
-    allvm-analysis = {
-      path = "default.nix";
-      input = "allvm-analysis-src";
-      description = "ALLVM Analysis Tools (pinned nixpkgs)";
-      shares = 100;
-    };
-    allvm-analysis-master = {
-      path = "default.nix";
-      input = "allvm-analysis-src";
-      description = "ALLVM Analysis Tools (nixpkgs master)";
-      inputs.nixpkgs = nixpkgs-master;
-      interval = 60 * 60 * 4;
+    allplay = {
+      default = {
+        path = "default.nix";
+        input = "allvm-analysis-src";
+        description = "ALLVM Analysis Tools (pinned nixpkgs)";
+        shares = 100;
+      };
+      with-nixpkgs-master = {
+        path = "default.nix";
+        input = "allvm-analysis-src";
+        description = "ALLVM Analysis Tools (nixpkgs master)";
+        inputs.nixpkgs = nixpkgs-master;
+        interval = 60 * 60 * 4;
+      };
     };
 
-    allvm-tools-llvm5 = {
-      path = "default.nix";
-      input = "allvm-tools-src";
-      inputs.allvm-tools-src = allvm-tools-llvm5;
-      description = "ALLVM Tools - LLVM5 (pinned nixpkgs)";
-    };
-    allvm-tools-llvm6 = {
-      path = "default.nix";
-      input = "allvm-tools-src";
-      inputs.allvm-tools-src = allvm-tools-llvm6;
-      description = "ALLVM Tools - LLVM6 (pinned nixpkgs)";
-    };
 
     ## Build allvm-tools in various cross configurations (as well as native), using various musl branches
-    allvm-tools-cross-pr = {
-      inputs.nixpkgs = nixpkgs-musl-pr;
-    };
-    allvm-tools-cross-staging = {
-      inputs.nixpkgs = nixpkgs-musl-staging;
-    };
-    allvm-tools-cross-cleanup = {
-      inputs.nixpkgs = nixpkgs-musl-cleanup;
-    };
-    allvm-tools-cross-pr-v6 = {
-      inputs.nixpkgs = nixpkgs-musl-pr-v6;
-    };
-    allvm-tools-llvm5-pr = {
-      inputs.nixpkgs = nixpkgs-musl-pr;
-      inputs.llvmVersion = { type = "nix"; value = "5"; };
-    };
-    allvm-tools-llvm5-pr-v6 = {
-      inputs.nixpkgs = nixpkgs-musl-pr-v6;
-      inputs.llvmVersion = { type = "nix"; value = "5"; };
-    };
-    allvm-tools-llvm5-staging = {
-      inputs.nixpkgs = nixpkgs-musl-staging;
-      inputs.llvmVersion = { type = "nix"; value = "5"; };
+    cross-and-native = {
+      default = {
+        with-musl-pr = {
+          inputs.nixpkgs = nixpkgs-musl-pr;
+        };
+        with-staging = {
+          inputs.nixpkgs = nixpkgs-musl-staging;
+        };
+        with-cleanup = {
+          inputs.nixpkgs = nixpkgs-musl-cleanup;
+        };
+      };
+      llvm5 =  {
+        with-musl-pr = {
+          inputs.nixpkgs = nixpkgs-musl-pr;
+          inputs.allvm-tools-src = allvm-tools-llvm5;
+          inputs.llvmVersion = { type = "nix"; value = "5"; };
+        };
+        with-staging = {
+          inputs.nixpkgs = nixpkgs-musl-staging;
+          inputs.allvm-tools-src = allvm-tools-llvm5;
+          inputs.llvmVersion = { type = "nix"; value = "5"; };
+        };
+        with-cleanup = {
+          inputs.nixpkgs = nixpkgs-musl-cleanup;
+          inputs.allvm-tools-src = allvm-tools-llvm5;
+          inputs.llvmVersion = { type = "nix"; value = "5"; };
+        };
+      };
+      llvm6 = {
+        with-musl-pr = {
+          inputs.nixpkgs = nixpkgs-musl-pr;
+          inputs.allvm-tools-src = allvm-tools-llvm6;
+          inputs.llvmVersion = { type = "nix"; value = "6"; };
+        };
+        with-staging = {
+          inputs.nixpkgs = nixpkgs-musl-staging;
+          inputs.allvm-tools-src = allvm-tools-llvm6;
+          inputs.llvmVersion = { type = "nix"; value = "6"; };
+        };
+        with-cleanup = {
+          inputs.nixpkgs = nixpkgs-musl-cleanup;
+          inputs.allvm-tools-src = allvm-tools-llvm6;
+          inputs.llvmVersion = { type = "nix"; value = "6"; };
+        };
+      };
     };
   });
 in writeSpec
