bd3a604b87c8bb7d96fcd12cbef1e4762e91f729
Will Dietz <w@wdtz.org>
2018-03-19 14:48:40 -0500

    default to supporting 32bit as well

--- support/default-settings.nix
+++ support/default-settings.nix
@@ -14,7 +14,7 @@
     };
     supportedSystems = {
       type = "nix";
-      value = ''[ \"x86_64-linux\" ]'';
+      value = ''[ \"x86_64-linux\" \"i686-linux\" ]'';
     };
   };
   mail = false;
